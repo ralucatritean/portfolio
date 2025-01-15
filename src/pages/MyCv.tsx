201dd7fafc57ce9c1f4020db0c1f8b8af76c3114
ralucatritean <ralucatritean@gmail.com>
2025-01-16 00:14:48 +0200

    changes:
    -fixed icon
    -fixed content

--- src/pages/MyCv.tsx
+++ src/pages/MyCv.tsx
@@ -129,10 +129,10 @@ function MyCvPage() {
             <div className="footer">
                 <div className="center-footer">
                     <div className="left-footer">
-                        <p><img className='iconImage' src={phone} alt="phone" /> +4071503030</p>
-                        <p><img className='iconImage' src={mail} alt="mail" /> ralucatritean@gmail.com</p>
-                        <p><img className='iconImage' src={linkedin} alt="linkedin" /> linkedin.com/in/raluca-grama</p>
-                        <p><img className='iconImage' src={adress} alt="adress" /> Târgu Mureș, Mureș, Romania</p>
+                        <p><img className='iconFooterImage' src={phone} alt="phone" /> +4071503030</p>
+                        <p><img className='iconFooterImage' src={mail} alt="mail" /> ralucatritean@gmail.com</p>
+                        <p><img className='iconFooterImage' src={linkedin} alt="linkedin" /> linkedin.com/in/raluca-grama</p>
+                        <p><img className='iconFooterImage' src={adress} alt="adress" /> Târgu Mureș, Mureș, Romania</p>
                     </div>
                 </div>
 
