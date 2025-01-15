781b225dc2cbcb8e7173321f9627cd3d01b32945
ralucatritean <ralucatritean@gmail.com>
2025-01-15 16:18:48 +0200

    update configuration files for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/', // Adăugăm baza pentru GitHub Pages
   build: {
     rollupOptions: {
       input: './src/index.tsx'
-    }
+    },
+    outDir: 'dist', // Specificăm directorul de output
+    assetsDir: 'assets'
   }
 })
